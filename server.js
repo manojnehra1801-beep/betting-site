2e8c5786032325ada789d3da476d82967fee2cfe
manojnehra1801-beep <manojnehra1801@gmail.com>
2026-07-16 21:04:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,33 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
+// Home Page
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
+});
+
+// Login Page
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+// Signup Page
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+// Login Form
+app.post("/login", (req, res) => {
+    res.send("Login Successful");
+});
+
+// Signup Form
+app.post("/signup", (req, res) => {
+    res.send("Signup Successful");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
